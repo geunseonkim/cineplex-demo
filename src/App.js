6aa55966a9ea54c756c420316e19cb8e38b13522
geunseonkim <guensoen@gmail.com>
2024-04-06 23:48:47 -0700

    add the NotFoundpage Route

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
           </Route>
         </Route>
 
-        <Route path="*" element={<NotFound/>}/>
+        <Route path="*" element={<NotFound/>}></Route>
 
       </Routes>
     </div>
