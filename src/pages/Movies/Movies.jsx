d72903e612e0389d503e1187117d436a463aa1cc
geunseonkim <guensoen@gmail.com>
2024-04-12 23:31:06 -0400

    create movie filter/genre button

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -5,8 +5,8 @@ import MovieCard from '../../common/MovieCard/MovieCard'
 import { Alert, Container, Row, Col} from 'react-bootstrap';
 import ReactPaginate from 'react-paginate';
 import './Movies.style.css';
-import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import { useMovieGenreQuery } from '../../hooks/useMovieGenre';
 
 // 2가지 경로.
 // - navbar 를 통해. => popularMovie
@@ -21,34 +21,80 @@ const Movies = () => {
   const [query, setQuery] = useSearchParams();
   const [page, setPage] = useState(1)
   const keyword = query.get("q");
-  
+  const {data:genreData} = useMovieGenreQuery()
+  const [movieGenre, setMovieGenre] = useState([])
+
   const handlePageClick = ({selected}) => {
     setPage(selected + 1);
   }
 
+  const sortByGenre = (genreId) => {
+    const genreList = data.results.filter((movie)=>(movie.genre_ids.includes(genreId)))
+    console.log("movie-ggg", genreList);
+    setMovieGenre(genreList)
+  }
+  const genreIdList = [28, 12, 16, 35, 80, 99, 18, 10751, 14, 36, 27, 10402, 9648, 10749, 878, 10770, 53, 10752, 37]
+
   const {data, isLoading, isError, error} = useSearchMovieQuery({keyword, page});
+
+  const dataHandlePopular = () => {
+    const handlePopularResult = data.results.sort((a, b)=> a.vote_average - b.vote_average)
+    console.log("ppp", handlePopularResult);
+    setMovieGenre(handlePopularResult);
+  }
+
+  const dataHandleLatest = () => {
+    const handleLatestResult = data.results.sort((a, b)=> a.release_date - b.release_date)
+    console.log("lll", handleLatestResult); 
+    setMovieGenre(handleLatestResult);
+  }
+
   if(isLoading) {return <h1>Loading ...</h1>}
   if(isError) {return <Alert variant="danger">{error.message}</Alert>}
 
   console.log("aaa", data);
+
+  const handlePopular = () => {
+    const value = "Most Popular Movies"
+    switch (value) {
+      case "Most Popular Movies" :
+        return dataHandlePopular();
+      default:
+    }
+  }
+
+  const handleLatest = () => {
+    const value = "The Latest Movies"
+    switch (value) {
+      case "The Latest Movies" :
+        return dataHandleLatest();
+      default:
+    }
+  }
+
 
   return (
     <Container style={{marginTop: "2.5rem"}}>
       <Row>
         <Col lg={4} xs={12}>
         <h4 className='category-name'>More to explore</h4>
-          <Button variant="outline-light" className='category-button-name'>Most Popular Movies</Button>
-          <Button variant="outline-light" className='category-button-name'>The Latest Movies</Button>
+          <Button variant="outline-light" className='category-button-name' onClick={()=>handlePopular()}>Most Popular Movies</Button>
+          <Button variant="outline-light" className='category-button-name' onClick={()=>handleLatest()}>The Latest Movies</Button>
 
           <h4 className='category-name'>Top Rated Movies by Genre &#62;</h4>
-          <div> Space for genre</div>
+          <div>
+            {genreIdList.map((genreId, idx)=><Button key={idx} onClick={()=>sortByGenre(genreId)} variant="outline-light" className='movie-genreList'>
+              {genreData.find((genre)=>(genre.id === genreId)).name}</Button>)}</div>
         </Col>
 
-        {/* pagination */}
+
         <Col lg={8} xs={12}>
           <Row className='movie-list'>
-          {data?.results.map((movie,idx)=><Col key={idx} lg={4} xs={12} style={{marginBottom: "2rem"}}><MovieCard movie={movie}/></Col>)}
+          {(movieGenre.length !== 0)? movieGenre.map((movie,idx)=><Col key={idx} lg={4} xs={12} style={{marginBottom: "2rem"}}><MovieCard movie={movie}/></Col>)
+         : data.results.map((movie, idx)=><Col key={idx} lg={4} xs={12} style={{marginBottom: "2rem"}}><MovieCard movie={movie}/></Col>) }
           </Row>
+
+         {/* pagination */}
           <ReactPaginate
             previousLabel="&#60;"
             nextLabel="&#62;"
